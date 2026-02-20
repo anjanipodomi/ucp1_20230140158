893b2739b71f30fd3860bd6df4c712a0ed7c8d65
anjanipodomi <anjanidihapsari@gmail.com>
2026-02-20 20:06:55 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Modul Pertemuan 1</title>
+</head>
+<body style="background-color:black; color:white; display:flex; justify-content:center; align-items:center; height:100vh; font-family:sans-serif;">
+    <div style="background:#111; padding:40px; border-radius:10px; width:400px;">
+        <h2>Nama: Anjani Dihapsari Podomi</h2>
+        <p>NIM: 20230140158</p>
+        <button style="margin-top:15px; padding:8px 15px; border:none; border-radius:5px;">
+            Modul Pertemuan 1
+        </button>
+    </div>
+</body>
+</html>
