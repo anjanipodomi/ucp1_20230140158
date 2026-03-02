149389f26ff9ff0d63f143f23569eb70619122c1
anjanipodomi <anjanidihapsari@gmail.com>
2026-03-02 12:38:36 +0700

    Membuat konfirmasi database dan menginstal Laravel Breeze untuk sistem autentikasi (Login/Register)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white text-gray-900 overflow-hidden shadow-md sm:rounded-lg p-6">
+                <h2 class="text-2xl font-bold mb-4">Biodata</h2>
+                <p><strong>Nama:</strong> Anjani Dihapsari Podomi</p>
+                <p><strong>NIM:</strong> 20230140158</p>
+                <p><strong>Program Studi:</strong> Teknologi Informasi</p>
+                <p><strong>Hobi:</strong> Denger Musik</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
